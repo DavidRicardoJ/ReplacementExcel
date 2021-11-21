7647e649a243ca8071cb7556a1603da42f2fce08
José Ricardo David <david_Ricardo@live.com>
2021-11-21 19:32:28 -0300

    Adicionando componentes de interface

---
+++ ReplacementExcel/Models/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace ReplacementExcel.Models
+{
+    public class AppSettings
+    {
+        public int FirstCollumn { get; set; }
+        public int FirstRow { get; set; }
+    }
+}
---
+++ ReplacementExcel/Models/ReposicaoEstoque.cs
@@ -0,0 +1,13 @@
+﻿namespace ReplacementExcel.Models
+{
+    public class ReposicaoEstoque
+    {
+        public string Codigo { get; set; }
+        public string Medicamento { get; set; }
+        public string Unidade { get; set; }
+        public DateTime UltimoMovimento { get; set; }
+        public float ConsumoTotal { get; set; }
+        public float EstoqueAtual { get; set; }
+        public int DiasDeEstoque { get; set; }
+    }
+}
